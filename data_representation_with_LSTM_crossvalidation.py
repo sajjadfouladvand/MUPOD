1a3bd7dddab33edd9196ba571b17cad637881053
Sajjad Fouladvand <sajjad@Sajjads-MacBook-Pro.local>
2021-04-30 00:01:26 -0400

    New Commit

--- data_representation_with_LSTM_crossvalidation.py
+++ data_representation_with_LSTM_crossvalidation.py
@@ -17,7 +17,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 
-WRITTEN BY Sajjad Fouladvand (sjjd.fouladvand@gmail.com) based on Google implementation of Transformer here: https://github.com/tensorflow/docs/blob/master/site/en/tutorials/text/transformer.ipynb
 
 """
 
